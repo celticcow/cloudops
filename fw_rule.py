99507d597fc04576121deacd3f9d66f5732863ea
Celticcow <gregory.dunlap@gmail.com>
2020-09-21 12:36:07 -0500

    init file with main

--- fw_rule.py
+++ fw_rule.py
@@ -1,1 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  -W ignore::DeprecationWarning
+
+import requests
+import json
+import sys
+import csv
+import time
+import getpass
+import ipaddress
+import argparse
+import apifunctions
+
+#remove the InsecureRequestWarning messages
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+"""
+test code to try and create simple http repy of data with json
+"""
+
+def main():
+    print("in main function")
+    debug = 1
+
+if __name__ == "__main__":
+    main()
